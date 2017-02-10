2b3b8178d3d976d326e816cf87342743b9e75e2f
Nayna Nayate <nayna.nayate@ASC-C02LM2A5FD58.local>
2017-02-10 10:10:37 -0600

    posted message to mlab dbase

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,24 @@
 import express from 'express';
+const app = express();
 import bodyParser from 'body-parser';
+const  MongoClient = require('mongodb').MongoClient;
 import path from 'path';
 import open from 'open';
-
 const port = 3000;
-const app = express();
+
+var db;
+MongoClient.connect('mongodb://nayna:nayna@ds135689.mlab.com:35689/dev', (err, database) =>{
+  if(err){
+    return console.log(err);
+  }
+  db= database;
+  // app.listen(port, () =>{
+  //   console.log('listening on '+ port);
+  // });
+
+});
+
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res)=> {
@@ -12,10 +26,15 @@ app.get('/', (req, res)=> {
 });
 
 app.post('/quotes', (req,res)=>{
+  db.collection('quotes').save(req.body, (err, result) =>{
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/');
+  });
   console.log(req.body);
-})
+});
 
-app.listen(port, function(err){
+app.listen(port, (err) => {
   if(err){
     console.log(err);
   }
