15b2a82cc9c40f18d6d297abe7e4c76e67c77840
Nayna Nayate <nayna.nayate@ASC-C02LM2A5FD58.local>
2017-02-10 18:20:35 -0600

    modified quotes CRUD, to a REST API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,7 +4,12 @@ import bodyParser from 'body-parser';
 const  MongoClient = require('mongodb').MongoClient;
 import path from 'path';
 import open from 'open';
-const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
 
 var db;
 
@@ -12,41 +17,66 @@ MongoClient.connect('mongodb://nayna:nayna@ds135689.mlab.com:35689/dev', (err, d
   if(err) return console.log(err);
 
   db = database;
+  console.log("Database connection ready");
 
   app.listen(port, (err) => {
-    if(err){
-      console.log(err);
-    }
-    else{
-      open('http://localhost:' + port);
-    }
-  });
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log('Magic happens on port ' + port);
+        open('http://localhost:' + port);
+      }
+    });
+
 
 
 });
 
 
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res)=> {
 
-  db.collection('quotes').find().toArray((err, result) => {
-    if(err) return console.log(err);
+var router = express.Router();
 
-    console.log(result);
-    
+router.use((req,res, next) => {
+  console.log('something is happening.');
+  next();
 
-  });
-  res.sendFile(path.join(__dirname,  '../src/index.html'));
 });
 
+router.get('/', (req, res)=> {
 
+  res.json({message: 'Yahoo!!'});
 
-app.post('/quotes', (req,res)=>{
+  // db.collection('quotes').find().toArray((err, result) => {
+  //   if(err) return console.log(err);
+  //
+  //   console.log(result);
+  //
+  //
+  // });
+  //res.sendFile(path.join(__dirname,  '../src/index.html'));
+});
+
+router.route('/quotes')
+.post((req,res)=>{
   db.collection('quotes').save(req.body, (err, result) =>{
-    if (err) return console.log(err);
-    console.log('saved to database');
-    res.redirect('/');
+       if (err) res.send(err);
+     res.json({message:'saved to database'});
+  //     res.redirect('/');
+   });
+})
+
+.get((req, res)=> {
+  db.collection('quotes').find().toArray((err, result) => {
+    if(err) res.send(err);
+    res.json(result);
   });
-  //console.log(req.body);
 });
+
+
+app.use('/api', router);
+// app.post('/quotes', (req,res)=>{
+//
+//   //console.log(req.body);
+// });
