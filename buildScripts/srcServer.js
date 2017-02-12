4449a281bf3787abd77c363078153ac5b6c42578
Nayna Nayate <nayna.nayate@ASC-C02LM2A5FD58.local>
2017-02-11 22:38:25 -0600

    rest crud

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 const  MongoClient = require('mongodb').MongoClient;
 import path from 'path';
 import open from 'open';
+var mongodb = require('mongodb');
+var ObjectID = mongodb.ObjectID;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -44,6 +46,11 @@ router.use((req,res, next) => {
 
 });
 
+function handleError(res, reason, message, code){
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
 router.get('/', (req, res)=> {
 
   res.json({message: 'Yahoo!!'});
@@ -61,22 +68,43 @@ router.get('/', (req, res)=> {
 router.route('/quotes')
 .post((req,res)=>{
   db.collection('quotes').save(req.body, (err, result) =>{
-       if (err) res.send(err);
-     res.json({message:'saved to database'});
+       if (err) handleError(res, err.message, "Failed to create new quote");
+       else res.json({message:'saved to database'});
   //     res.redirect('/');
    });
 })
 
 .get((req, res)=> {
   db.collection('quotes').find().toArray((err, result) => {
-    if(err) res.send(err);
-    res.json(result);
+    if (err) handleError(res, err.message, "Failed to get contacts.");
+    else res.status(200).json(result);
   });
 });
 
+router.route('/quotes/:id')
+ .get((req,res)=>{
+   db.collection('quotes').findOne({_id: new ObjectID(req.params.id)}, (err, result) => {
+     if (err) handleError(res, err.message, "Failed to get contact.");
+     else res.status(200).json(result);
+   })
+
+ })
+ .put((req,res)=>{
+   var body = req.body;
+   delete body._id;
+   db.collection('quotes').updateOne({_id: new ObjectID(req.params.id)},body, (err,result) =>{
+     if(err) handleError(res, err.message, "Failed to update contact.");
+     else res.status(200).json('The record was updated');
+   })
+ })
+ .delete((req,res)=>{
+   db.collection('quotes').deleteOne({_id: new ObjectID(req.params.id)}, (err, result)=>{
+     if(err) handleError(res, err.message, "Failed to delete contact.");
+     res.status(204).json('The record was deleted.');
+
+   })
+
+ })
+
 
 app.use('/api', router);
-// app.post('/quotes', (req,res)=>{
-//
-//   //console.log(req.body);
-// });
